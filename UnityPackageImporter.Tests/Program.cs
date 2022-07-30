c1c7934b68c9cd01f8244503dbf364a0b4d06d84
dfgHiatus <51272212+dfgHiatus@users.noreply.github.com>
2022-07-30 09:20:39 -0500

    Start of YAML parsing

---
+++ UnityPackageImporter.Tests/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace UnityPackageImporter.Tests
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
