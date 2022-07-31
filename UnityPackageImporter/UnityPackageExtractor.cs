a0d76264cabe416b9a31408038f0e7cdab6ce051
dfgHiatus <51272212+dfgHiatus@users.noreply.github.com>
2022-07-30 21:25:15 -0500

    fixed a very weird bug. now returns all files in one path tho

--- UnityPackageImporter/UnityPackageExtractor.cs
+++ UnityPackageImporter/UnityPackageExtractor.cs
@@ -1,10 +1,13 @@
-﻿using System.IO;
+﻿using BaseX;
+using System.IO;
 using System.IO.Compression;
 
 namespace UnityPackageImporter.Extractor
 {
     public class UnityPackageExtractor
     {
+        private static readonly char[] theAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".ToCharArray();
+
         public void Unpack(string input, string outputDir)
         {
             var a = File.OpenRead(input);
@@ -17,10 +20,8 @@ namespace UnityPackageImporter.Extractor
                     break;
                 
                 string dirPath = Path.Combine(temp, tarEntry.Name);
-                // If the data length is 0, this is a directory entry
-                if (tarEntry.Data.Length == 0)
+                if (tarEntry.Data.Length == 0) // If the data length is 0, this is a directory entry
                 {
-                    // Create new dir if it doesn't exist
                     if (!Directory.Exists(dirPath))
                         Directory.CreateDirectory(dirPath);
                 }
@@ -30,12 +31,22 @@ namespace UnityPackageImporter.Extractor
             
             b.Close();
             a.Close();
-            foreach (var dir in Directory.GetDirectories(temp)) // Get all dirs in extracted file
+            foreach (var dir in Directory.GetDirectories(temp))
             {
                 var assetPath = Path.Combine(dir, "asset");
-                var pathName = Path.Combine(dir, "pathname");
-                if (!File.Exists(assetPath) || !File.Exists(pathName)) continue;
-                var fileName = Path.GetFileName(File.ReadAllText(pathName).Trim()); // Stinky => var fileName = Path.GetFileName(File.ReadAllLines(pathName)[0]);
+                var rawPathName = Path.Combine(dir, "pathname");
+                if (!File.Exists(assetPath) || !File.Exists(rawPathName)) continue;
+
+                // Essentially, we are given a string with a lot of added spaces that represents a path to an asset
+                // The easiest thing to do is Trim() the string (which worked for v1.0.0 and v1.1.0?!), but for some
+                // eldritch reason it no longer wants to remove trailing spaces. I love this language.
+                var rawText = File.ReadAllText(rawPathName);
+                UniLog.Log(rawText);
+                var lastIndex = rawText.LastIndexOfAny(theAlphabet);
+                var pathName = rawText.Substring(0, lastIndex + 1);
+                UniLog.Log(pathName);
+
+                var fileName = Path.GetFileName(pathName); 
                 var outFile = Path.Combine(outputDir, fileName);
                 if (!File.Exists(outFile)) File.Copy(assetPath, outFile);
             }
