c48a738c056d24031a56804103b4ff0a86cfa402
dfgHiatus <51272212+dfgHiatus@users.noreply.github.com>
2022-08-06 10:49:55 -0500

    Bump tests

--- UnityPackageImporter.Tests/Program.cs
+++ UnityPackageImporter.Tests/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace UnityPackageImporter.Tests
+﻿using Chimera.Unity3DYamlChecker;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+using UnityEngine;
+
+namespace UnityPackageImporter.Tests
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var deserializer = new DeserializerBuilder()
+                .WithNamingConvention(UnderscoredNamingConvention.Instance)  // see height_in_inches in sample yml 
+                .Build();
+            var yml = ""; // TODO Load
+            // var p = deserializer.Deserialize<GameObject>(yml);
+
+            UnityNodeTypeResolver res = new UnityNodeTypeResolver();
         }
     }
 }
